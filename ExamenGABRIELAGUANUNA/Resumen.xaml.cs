6ffe73a7c8da62471741bd95ea73b30d10a13d3c
Gabucha25 <gaby.guanuna25@gmail.com>
2021-04-27 23:28:16 -0500

    Agregar archivos de proyecto.

---
+++ ExamenGABRIELAGUANUNA/Resumen.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExamenGABRIELAGUANUNA
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resumen : ContentPage
+    {
+        public Resumen(string userlogin, string nombre, string tot)
+        {
+            InitializeComponent();
+
+            lblUser.Text = userlogin;
+            txtEstudiante.Text = nombre;
+            txtTotal.Text = tot;
+        }
+    }
+}
